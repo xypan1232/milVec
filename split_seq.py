ed9263a7547d7b7b9fc305b31625e78904c588c0
xypan1232 <xypan172436@gmail.com>
2017-03-13 15:16:36 +0100

    Update split_seq.py

--- split_seq.py
+++ split_seq.py
@@ -11,7 +11,52 @@ def padding_sequence_new(seq, max_len = 101, repkey = 'N'):
         new_seq = seq + repkey * gap_len
     return new_seq
 
+def read_rna_dict(rna_dict = 'rna_dict'):
+    odr_dict = {}
+    with open(rna_dict, 'r') as fp:
+        for line in fp:
+            values = line.rstrip().split(',')
+            for ind, val in enumerate(values):
+                val = val.strip()
+                odr_dict[val] = ind
+    
+    return odr_dict
+
+def get_6_trids():
+    nucle_com = []
+    chars = ['A', 'C', 'G', 'U']
+    base=len(chars)
+    end=len(chars)**6
+    for i in range(0,end):
+        n=i
+        ch0=chars[n%base]
+        n=n/base
+        ch1=chars[n%base]
+        n=n/base
+        ch2=chars[n%base]
+        n=n/base
+        ch3=chars[n%base]
+        n=n/base
+        ch4=chars[n%base]
+        n=n/base
+        ch5=chars[n%base]
+        nucle_com.append(ch0 + ch1 + ch2 + ch3 + ch4 + ch5)
+    return  nucle_com 
+
+def get_embed_dim_new(embed_file):
+    with open(embed_file) as f:
+        pepEmbedding = pickle.load(f)
         
+    embedded_dim = pepEmbedding[0].shape
+    print embedded_dim
+    n_aa_symbols, embedded_dim = embedded_dim
+    print n_aa_symbols, embedded_dim
+    # = embedded_dim[0]
+    embedding_weights = np.zeros((n_aa_symbols + 1,embedded_dim))
+    embedding_weights[1:,:] = pepEmbedding[0]
+    
+    return embedded_dim, pepEmbedding[0], n_aa_symbols
+
 def split_overlap_seq(seq):
     window_size = 101
     overlap_size = 20
@@ -37,7 +82,15 @@ def split_overlap_seq(seq):
             pad_seq = padding_sequence_new(seq1)
             print pad_seq
 
-        
+ def get_all_embedding():
+    trids =  get_6_trids()
+    ord_dict = read_rna_dict()
+    embedded_rna_dim, embedding_rna_weights, n_nucl_symbols = get_embed_dim_new('rnaEmbedding25.pickle')
+
+    for key, val in pairs.iteritems():
+        ind1 = trids.index(key)
+        emd_weight1 = embedding_rna_weights[ord_dict[str(ind1)]]
+
 seq= 'TTATCTCCTAGAAGGGGAGGTTACCTCTTCAAATGAGGAGGCCCCCCAGTCCTGTTCCTCCACCAGCCCCACTACGGAATGGGAGCGCATTTTAGGGTGGTTACTCTGAAACAAGGAGGGCCTAGGAATCTAAGAGTGTGAAGAGTAGAGAGGAAGTACCTCTACCCACCAGCCCACCCGTGCGGGGGAAGATGTAGCAGCTTCTTCTCCGAACCAA'
 print len(seq)
 split_overlap_seq(seq)
