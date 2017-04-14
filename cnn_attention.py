508edc558c9a2b20941c9c5887eef6ff1800d784
xypan1232 <xypan172436@gmail.com>
2017-04-14 14:46:49 +0200

    Update cnn_attention.py

--- cnn_attention.py
+++ cnn_attention.py
@@ -83,3 +83,29 @@ def build_model(opts, verbose=False):
     # model.compile(loss={'output':'categorical_crossentropy'}, optimizer=Adam(options.lr))
     model.compile(loss='categorical_crossentropy',optimizer=Adam(options.lr))
     return model
+
+
+def new_attention():
+
+      input_dim = 32
+      hidden = 32
+
+      #The LSTM  model -  output_shape = (batch, step, hidden)
+      model1 = Sequential()
+      model1.add(LSTM(input_dim=input_dim, output_dim=hidden, input_length=step, return_sequences=True))
+
+      #The weight model  - actual output shape  = (batch, step)
+      # after reshape : output_shape = (batch, step,  hidden)
+      model2 = Sequential()
+      model2.add(Dense(input_dim=input_dim, output_dim=step))
+      model2.add(Activation('softmax')) # Learn a probability distribution over each  step.
+      #Reshape to match LSTM's output shape, so that we can do element-wise multiplication.
+      model2.add(RepeatVector(hidden))
+      model2.add(Permute(2, 1))
+
+      #The final model which gives the weighted sum:
+      model = Sequential()
+      model.add(Merge([model1, model2], 'mul'))  # Multiply each element with corresponding weight a[i][j][k] * b[i][j]
+      model.add(TimeDistributedMerge('sum')) # Sum the weighted elements.
+
+      model.compile(loss='mse', optimizer='sgd')
